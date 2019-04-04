4740d82ed6674cf008a6d81bcac0d701c3319a8a
Ivan Doronin <dwnnak@gmail.com>
2019-04-04 11:07:40 +0500

    Update TerrainGenerator.cs

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -1,5 +1,79 @@
 ﻿using Application;
 using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class TreeInfo
+{
+    public static float maxScale;
+    public static float minScale;
+    public static int countCast;
+    TreeInstance tree;
+    public TreeInstance Tree
+    {
+        get
+        {
+            return tree;
+        }
+        private set
+        {
+            tree = value;
+        }
+    }
+    public float aloneRadius;
+    public float parentRadius;
+    int castType;
+
+    int CastType(float scale)
+    {
+        float castZone = maxScale - minScale;
+        return 1 - (int)(countCast * (tree.heightScale - minScale) / castZone);
+    }
+
+    //конструктор в котором радиус одиночества и родительский радиус зависят от размеров дерева scale
+    public TreeInfo(Vector3 pos, int prototypeIndex, float scale)
+    {
+        tree.position = pos;
+        tree.prototypeIndex = prototypeIndex;
+        tree.heightScale = scale;
+        tree.widthScale = scale;
+        tree.lightmapColor = Color.white;
+        tree.color = Color.white;
+        aloneRadius = scale * 0.02f;
+        parentRadius = scale * 0.5f;
+        castType = CastType(scale);
+
+    }
+
+    public TreeInfo(Vector3 pos, int prototypeIndex, float scale, float aloneR = 1, float parentR = 10)
+        : this(pos, prototypeIndex, scale)
+    {
+        aloneRadius = aloneR;
+        parentRadius = parentR;
+    }
+
+    public static implicit operator Vector3(TreeInfo a)
+    {
+        return a.tree.position;
+    }
+
+    public static implicit operator TreeInfo(Vector3 a)
+    {
+        return new TreeInfo(a, 0, 0);
+    }
+
+    public static implicit operator Vector2(TreeInfo a)
+    {
+        Vector2 result = new Vector2(a.tree.position.x, a.tree.position.z);
+        return result;
+    }
+
+    public static implicit operator TreeInstance(TreeInfo a)
+    {
+        return a.tree;
+    }
+}
+
 
 public class TerrainGenerator : MonoBehaviour
 {
@@ -20,19 +94,189 @@ public class TerrainGenerator : MonoBehaviour
     public float flatCoefficient = 0.2f;    //сглаживание шума
     private float[,] heights;
 
+    Terrain terrain;
+
+    //Владение гошана
+    public List<List<TreeInfo>> trees = new List<List<TreeInfo>>();
+    public List<Vector3> questZones; // x = x; y = radius; z = z
+    private float xTerrain = 0;
+    private float zTerrain = 0;
+    public int casts;
+    private int castCount;
+
+    public int CastCount
+    {
+        get { return castCount; }
+        set
+        {
+            castCount = value;
+            if (value > 8)
+            {
+                castCount = 8;
+            }
+            else if (value < 2)
+            {
+                castCount = 2;
+            }
+        }
+    }
+
+    public int maxDist = 4;
+    public int minDist = 3;
+    public float maxTreeScale = 2;
+    public float minTreeScale = 1;
+    public int ind = 0;
+
+    bool checkQzone(Vector3 zone, Vector2 coord)
+    {
+        float r = zone.y;
+        if (zone.x + r > coord.x && zone.x - r < coord.x
+        && zone.z + r > coord.y && zone.z - r < coord.y)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool isQZones(List<Vector3> zone, Vector2 coord)
+    {
+        bool result = false;
+        for (int i = 0; i < zone.Count; i++)
+        {
+            if (checkQzone(zone[i], coord))
+            {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    bool isQZones(Vector2 coord)
+    {
+        return isQZones(questZones, coord);
+    }
+
+    void gCasts()
+    {
+        castCount = casts;
+        casts = castCount;
+        TreeInfo.countCast = casts;
+
+        for (int i = 0; i < casts; i++)
+        {
+            trees.Add(new List<TreeInfo>());
+        }
+    }
+
+    void DrawCastTree()
+    {
+        for (int i = 0; i < castCount; i++)
+        {
+            for (int j = 0; j < trees[i].Count; j++)
+            {
+                terrain.AddTreeInstance(trees[i][j]);
+            }
+        }
+        terrain.Flush();
+    }
+
+    void gTreesQuestZones(int cast)
+    {
+        for (int i = 0; i < trees[cast].Count; i++)
+        {
+            Vector3 AloneRadius = trees[cast][i];
+            AloneRadius.y = Math.Max(trees[cast][i].aloneRadius, minDist);
+            questZones.Add(AloneRadius);
+        }
+    }
+
+    int checkParent(int castIndex, Vector2 currentChildIndx, System.Random rn)
+    {
+        List<int> prIndexCollection = new List<int>();
+        if (castIndex > 1)
+        {
+            for (int i = 0; i < trees[castIndex - 1].Count; i++)
+            {
+                Vector3 param = trees[castIndex - 1][i];
+                param.y = trees[castIndex - 1][i].parentRadius;
+                if (checkQzone(param, currentChildIndx))
+                {
+                    prIndexCollection.Add(trees[castIndex - 1][i].Tree.prototypeIndex);
+                }
+            }
+        }
+
+        if (prIndexCollection.Count == 0)
+        {
+            return rn.Next(0, terrain.terrainData.treePrototypes.Length);
+        }
+
+        return prIndexCollection[rn.Next(prIndexCollection.Count)];
+    }
+
+    void addCastTree(int castIndx, System.Random rn)
+    {
+        var alphaMaps = terrain.terrainData.GetAlphamaps(0, 0, terrain.terrainData.alphamapWidth, terrain.terrainData.alphamapHeight);
+
+        int cTextureOnTerH = terrain.terrainData.alphamapHeight / height;
+        int cTextureOnTerW = terrain.terrainData.alphamapWidth / width;
+        int iTextureGraund = 1; /// индекс текстуры земли КОСТЫЛЬ
+        float cTS = (maxTreeScale - minTreeScale) / castCount;
+        float scale = (maxTreeScale - (castIndx * cTS)) * 1000;
+        int maxTS = (int)scale;
+        scale = (maxTreeScale - ((castIndx + 1) * cTS)) * 1000;
+        int minTS = (int)scale;
+
+        for (int x = 0; x < width - minDist; x += rn.Next(minDist, maxDist) * castCount)
+        {
+            for (int z = 0; z < height; z += rn.Next(minDist, maxDist) * castCount)
+            {
+                int noiseX = rn.Next(-(int)(minDist * 0.5f), (int)(minDist * 0.5f));
+                int xNoise = Math.Abs(x + noiseX);
+
+                Vector2 coord = new Vector2(xNoise, z);
+                if (!isQZones(coord) && (alphaMaps[z * cTextureOnTerW, xNoise * cTextureOnTerH, iTextureGraund] < 1))
+                {
+                    var position = new Vector3((float)xNoise / width, heights[xNoise, z], ((float)z) / height);
+                    int prototypeIndex = checkParent(castIndx, coord, rn);
+
+                    var tree = new TreeInfo(position, prototypeIndex, rn.Next(minTS, maxTS) * 0.001f);
+                    trees[castIndx].Add(tree);
+                }
+            }
+        }
+    }
+
+    void gTree(float xTer, float zTer)
+    {
+        gCasts();
+        for (int i = 0; i < castCount; i++)
+        {
+            System.Random rn = new System.Random();
+            addCastTree(i, rn);
+            gTreesQuestZones(i);
+        }
+        DrawCastTree();
+    }
+
+
+    // Конец власти Гошана
     private void Start()
     {
-        offsetX = Random.Range(0, 1000f);
-        offsetY = Random.Range(0, 1000f);
-        Terrain terrain = GetComponent<Terrain>();
+        TreeInfo.maxScale = maxTreeScale;
+        TreeInfo.minScale = minTreeScale;
+
+        offsetX = UnityEngine.Random.Range(0, 1000f);
+        offsetY = UnityEngine.Random.Range(0, 1000f);
+        //Terrain terrain = GetComponent<Terrain>();
         terrain.terrainData = CreateTerrain(terrain.terrainData);
 
     }
     private void Update()
     {
+        casts = castCount;
         //Terrain terrain = GetComponent<Terrain>();
         //terrain.terrainData = CreateTerrain(terrain.terrainData);
-
         // offsetX += Time.deltaTime * 2f;
     }
 
@@ -40,7 +284,8 @@ public class TerrainGenerator : MonoBehaviour
     {
         terrainData.heightmapResolution = width + 1;
         terrainData.size = new Vector3(width, plainDepth, height);
-        terrainData.SetHeights(0, 0, CreateHeights());              //генерация шумом перлина
+
+        terrainData.SetHeights((int)xTerrain,(int)zTerrain, CreateHeights());              //генерация шумом перлина
         //terrainData.SetHeights(0, 0, CreateHeightsRed());         //для экспериментов с шумами
         return terrainData;
     }
