fa1a849625d10f891229305814a1948268ba53d9
Ivan Doronin <dwnnak@gmail.com>
2019-04-04 10:31:53 +0500

    Update TerrainGenerator.cs

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -17,31 +17,35 @@ public class TerrainGenerator : MonoBehaviour
     public int mountainY = 100;
     public double angle = 30f;
 
-    public float flatCoefficient = 0.2f;
+    public float flatCoefficient = 0.2f;    //сглаживание шума
     private float[,] heights;
 
     private void Start()
     {
         offsetX = Random.Range(0, 1000f);
         offsetY = Random.Range(0, 1000f);
-    }
-    private void Update()
-    {
         Terrain terrain = GetComponent<Terrain>();
         terrain.terrainData = CreateTerrain(terrain.terrainData);
 
-      // offsetX += Time.deltaTime * 2f;
+    }
+    private void Update()
+    {
+        //Terrain terrain = GetComponent<Terrain>();
+        //terrain.terrainData = CreateTerrain(terrain.terrainData);
+
+        // offsetX += Time.deltaTime * 2f;
     }
 
     private TerrainData CreateTerrain(TerrainData terrainData)
     {
         terrainData.heightmapResolution = width + 1;
         terrainData.size = new Vector3(width, plainDepth, height);
-        terrainData.SetHeights(0, 0, CreateHeights());
+        terrainData.SetHeights(0, 0, CreateHeights());              //генерация шумом перлина
+        //terrainData.SetHeights(0, 0, CreateHeightsRed());         //для экспериментов с шумами
         return terrainData;
     }
 
-    private float [,] CreateHeights()
+    private float[,] CreateHeights()
     {
         heights = new float[width, height];
         for (int x = 0; x < width; x++)
@@ -54,9 +58,36 @@ public class TerrainGenerator : MonoBehaviour
         return heights;
     }
 
-    private float WhiteNoise()
+    private float[,] CreateHeightsRed()
     {
-        return Random.Range(0, plainDepth);
+        var _heights = new float[width+1, height+1];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                _heights[x, y] = Noise(x,y);
+            }
+        }
+       
+        return RedNoise(_heights);
+    }
+
+    private float Noise(int x, int y)
+    {
+        return Mathf.Sin(x+y) + Mathf.Cos(x+y);
+    }
+
+    private float[,] RedNoise(float[,] noise)
+    {
+        var _heights = new float[width, height];
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                _heights[i, j] = flatCoefficient * (noise[i, j] + noise[i, j + 1] + noise[i+1, j])/3;
+            }
+        }
+        return _heights;
     }
 
     private float CalculateHeight(int x, int y)
