e9b83b93b21a63e4f0fad82155a736ccab8fbc0c
DRed <dima.redkin.1999@gmail.com>
2019-05-15 14:32:59 +0500

    ochepyatka

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -93,7 +93,7 @@ public class TerrainGenerator : Generator
     private void SetOnes(int[,] array)
     {
         for (int i = 0; i < array.GetLength(0); i++)
-            for (int j = 0; i < array.GetLength(1); j++)
+            for (int j = 0; j < array.GetLength(1); j++)
                 array[i, j] = 1;
     }
 
